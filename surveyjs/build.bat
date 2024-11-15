7cd73cc05ec25b7f202c0b884405a96cef917e44
bing <david.bingham@nimbasolutions.com>
2024-11-15 10:14:26 -0700

    update build scripts

--- surveyjs/build.bat
+++ surveyjs/build.bat
@@ -1,5 +1,8 @@
 @echo off
 
+set ROOT=%~dp0..\
+set STATIC_RESOURCES=%ROOT%force-app\main\default\staticresources
+
 cd surveyjs/survey-library
 echo -----------------------------------------
 echo BUILDING survey-library SOURCES...
@@ -21,6 +24,16 @@ echo -----------------------------------------
 call npm run build
 
 echo -----------------------------------------
-echo All builds completed
+echo Copying build files to staticresources...
+echo -----------------------------------------
+xcopy /Y "%~dp0surveyjs\survey-library\build\survey-core\defaultV2.css" "%STATIC_RESOURCES%\"
+xcopy /Y "%~dp0surveyjs\survey-library\build\survey-core\survey.core.min.js" "%STATIC_RESOURCES%\"
+xcopy /Y "%~dp0surveyjs\survey-creator\packages\survey-creator-core\build\survey-creator-core.css" "%STATIC_RESOURCES%\"
+xcopy /Y "%~dp0surveyjs\survey-creator\packages\survey-creator-core\build\survey-creator-core.js" "%STATIC_RESOURCES%\"
+xcopy /Y "%~dp0surveyjs\survey-creator\packages\survey-creator-core\build\survey-creator-core.min.js" "%STATIC_RESOURCES%\"
+xcopy /Y "%~dp0surveyjs\survey-library\build\survey-js-ui\survey-js-ui.min.js" "%STATIC_RESOURCES%\"
+
+echo -----------------------------------------
+echo Build completed and files copied
 echo -----------------------------------------
 pause

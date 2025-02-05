89f5872258a84ffe71d6e123fe14ed05cd8a3733
Kartik-choudhary <Kartikc340@gmail.com>
2025-02-05 20:42:10 +0530

    Sync Latest version

---
+++ force-app/main/default/lwc/surveyEdit/surveyEdit.js
@@ -0,0 +1,22 @@
+import { LightningElement,api } from 'lwc';
+import getSurveyVersionBySurvey from '@salesforce/apex/SurveyBuilder.getSurveyVersionBySurvey';
+
+export default class SurveyEdit extends LightningElement {
+
+    @api invoke() {
+        let recordId = window.location.href.split('Survey__c/')[1].split('/')[0];
+
+        getSurveyVersionBySurvey({recId : recordId})
+        .then(result => {
+            
+            let builderUrl = window.location.href.split('lightning/')[0] + 'lightning/n/SurveyJS?surveyId__c=' + result[0].Id;
+
+            window.location.href = builderUrl;
+
+        }).catch(err=>{
+            console.log('error in fetching survey-'+err);
+            console.log('error in fetching survey-'+err.body);
+            console.log('error in fetching survey-'+err.message);
+        })
+    }
+}

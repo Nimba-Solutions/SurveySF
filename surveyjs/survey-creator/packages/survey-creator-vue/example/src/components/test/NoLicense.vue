78762a75933229282e41eac97c84db9437bbfe15
bing <david.bingham@nimbasolutions.com>
2025-03-12 10:21:24 -0600

    Clean workspace before adding subtrees

--- surveyjs/survey-creator/packages/survey-creator-vue/example/src/components/test/NoLicense.vue
+++
@@ -1,6 +0,0 @@
-<template>
-        <Common :options="{ expandCollapseButtonVisibility: 'never',  showLogicTab: true, showTranslationTab: true }" :use-slk="false"></Common>
-</template>
-<script lang="ts" setup>
-import Common from './Common.vue';
-</script>

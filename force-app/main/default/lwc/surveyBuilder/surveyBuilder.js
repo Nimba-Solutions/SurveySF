0854cdb8d5eae45f488a189cfa7521c7cda1bd5e
bing <david.bingham@nimbasolutions.com>
2025-03-24 15:36:07 -0600

    simplify the component

--- force-app/main/default/lwc/surveyBuilder/surveyBuilder.js
+++ force-app/main/default/lwc/surveyBuilder/surveyBuilder.js
@@ -1,9 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import saveSurvey from '@salesforce/apex/SurveyBuilderController.saveSurvey';
-import getLatestVersion from '@salesforce/apex/SurveyBuilderController.getLatestVersion';
-import getVersionById from '@salesforce/apex/SurveyBuilderController.getVersionById';
 import SURVEY_CORE from '@salesforce/resourceUrl/surveycore';
 import SURVEY_JS_UI from '@salesforce/resourceUrl/surveyjsui';
 import SURVEY_CORE_CSS from "@salesforce/resourceUrl/surveycoremin";
@@ -13,20 +10,21 @@ import SURVEY_CREATOR_JS from '@salesforce/resourceUrl/surveycreatormin';
 import SURVEY_INDEX_JS from '@salesforce/resourceUrl/indexmin';
 import DEFAULT_SURVEY_JSON from '@salesforce/resourceUrl/defaultSurveyJson';
 
+// APEX
+// import saveSurvey from '@salesforce/apex/SurveyBuilderController.saveSurvey';
+
 export default class SurveyBuilder extends LightningElement {
     surveyInitialized = false;
-    loaded = false;
+    @track isLoading = true;
     surveyJson = {};
-    disableSave = true;
-    surveyVersionRec;
-    encryptedSurveyId;
-    surveyVersionList = [];
-    showDropDown = false;
     notFound = false;
-    changesUnSaved = false;
-    siteDomain = '';
+    @track hasUnsavedChanges = false;
     creator = null;
     defaultSurveyJson;
+
+    get isSaveDisabled() {
+        return this.isLoading || !this.hasUnsavedChanges;
+    }
 
     connectedCallback() {
         const queryString = window.location.search;
@@ -34,31 +32,10 @@ export default class SurveyBuilder extends LightningElement {
         
         if(urlParams.has('versionId')) {
             const versionId = urlParams.get('versionId');
-            getVersionById({ versionId })
-                .then(result => {
-                    this.surveyVersionRec = { body: result };
-                    this.surveyJson = JSON.parse(result);
-                    this.initializeSurvey();
-                })
-                .catch(error => {
-                    console.error('Error loading version:', error);
-                    this.notFound = true;
-                });
+            console.log(`TODO: Load Survey Content for SurveyVersion__c.Id: ${versionId}`);
         } else if(urlParams.has('surveyId')) {
             const surveyId = urlParams.get('surveyId');
-            getLatestVersion({ surveyId })
-                .then(result => {
-                    this.surveyVersionRec = { body: result };
-                    this.surveyJson = JSON.parse(result);
-                    this.initializeSurvey();
-                })
-                .catch(error => {
-                    console.error('Error loading latest version:', error);
-                    this.notFound = true;
-                });
-        } else {
-            // If no parameters, initialize with default JSON
-            this.initializeSurvey();
+            console.log(`TODO: Load Survey Content for Survey__c.Id's latest SurveyVersion__c: ${surveyId}`);
         }
     }
 
@@ -79,15 +56,14 @@ export default class SurveyBuilder extends LightningElement {
         ])
         .then(([,,,,,,,defaultJson]) => {
             this.defaultSurveyJson = defaultJson;
-            this.loaded = true;
-            if (!this.surveyVersionRec) {
-                this.surveyJson = this.defaultSurveyJson;
-            }
+            this.isLoading = false;
+            this.surveyJson = this.defaultSurveyJson;
             this.initializeSurvey();
         })
         .catch(error => {
             console.error('Error loading resources:', error);
             this.showErrorToast('Error loading survey resources');
+            this.isLoading = false;
         });
     }
 
@@ -107,22 +83,9 @@ export default class SurveyBuilder extends LightningElement {
         creator.text = JSON.stringify(this.surveyJson);
         
         creator.saveSurveyFunc = (saveNo, callback) => {
-            this.disableSave = false;
-            this.changesUnSaved = true;
+            this.hasUnsavedChanges = true;
             this.surveyJson = JSON.parse(creator.text);
-            
-            saveSurvey({ jsonString: creator.text })
-                .then(result => {
-                    this.surveyJson = JSON.parse(result);
-                    this.changesUnSaved = false;
-                    this.showSuccessToast('Survey saved successfully');
-                    callback(saveNo, true);
-                })
-                .catch(error => {
-                    console.error('Error saving survey:', error);
-                    this.showErrorToast('Error saving survey: ' + (error.body?.message || error.message));
-                    callback(saveNo, false);
-                });
+            callback(saveNo, true);
         };
         
         creator.render(this.template.querySelector('.surveyContainer'));
@@ -130,85 +93,13 @@ export default class SurveyBuilder extends LightningElement {
 
     handleSave(event) {
         if(!this.surveyJson.title) {
-            this.disableSave = true;
             this.showErrorToast('Please provide survey title');
             return;
         }
 
-        saveSurvey({ jsonString: JSON.stringify(this.surveyJson) })
-            .then(result => {
-                this.surveyJson = JSON.parse(result);
-                this.changesUnSaved = false;
-                this.disableSave = true;
-                this.showSuccessToast('Survey saved successfully');
-                this.refreshVersion();
-            })
-            .catch(error => {
-                console.error('Error saving survey:', error);
-                this.showErrorToast('Error saving survey: ' + (error.body?.message || error.message));
-            });
-    }
-
-    handleActivate() {
-        saveVersionAsActive({ 
-            versionModel: {
-                ...this.surveyVersionRec,
-                body: JSON.stringify(this.surveyJson)
-            }
-        })
-        .then(() => {
-            this.surveyVersionRec = {
-                ...this.surveyVersionRec,
-                Status__c: 'Active'
-            };
-            this.showSuccessToast('Survey activated successfully');
-        })
-        .catch(error => {
-            this.showErrorToast('Error activating survey: ' + (error.body?.message || error.message));
-        });
-    }
-
-    refreshVersion() {
-        const surveyId = this.surveyVersionRec.Survey__c;
-        getLatestVersion({ surveyId })
-            .then(result => {
-                this.surveyVersionRec = { body: result };
-                this.surveyJson = JSON.parse(result);
-            })
-            .catch(error => {
-                console.error('Error refreshing version:', error);
-                this.showErrorToast('Error refreshing survey data');
-            });
-    }
-
-    copySurveyLink() {
-        navigator.clipboard.writeText(this.siteDomain + '/survey?surveyId=' + this.encryptedSurveyId);
-        this.showSuccessToast('Survey Link Copied to Clipboard!');
-    }
-
-    openVersion(event) {
-        const versionId = event.currentTarget.dataset.surveyid;
-        getVersionById({ versionId })
-            .then(result => {
-                this.surveyVersionRec = { body: result };
-                this.surveyJson = JSON.parse(result);
-                this.creator.text = result;
-            })
-            .catch(error => {
-                console.error('Error opening version:', error);
-                this.showErrorToast('Error opening survey version');
-            });
-    }
-
-    clickDropDown() {
-        this.showDropDown = !this.showDropDown;
-    }
-
-    get showActivate() {
-        if(this.surveyVersionRec) {
-            return this.surveyVersionRec.Status__c === 'Draft';
-        }
-        return false;
+        console.log('TODO: Implement SurveyService.saveSurvey()');
+        this.showSuccessToast('TO DO: Implement SurveyService.saveSurvey()');
+        this.hasUnsavedChanges = false;
     }
 
     showSuccessToast(message) {
